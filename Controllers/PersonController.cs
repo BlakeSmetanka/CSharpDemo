51f751c3a0e04fdc389117ba0e96a5cf5b768d62
pieguy40 <marthvader9@gmail.com>
2022-02-07 11:07:44 -0500

    Initial GitHub commit

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Demo.Models;
+
+namespace Demo.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index(PersonModel model)
+        {
+            return View(model);
+        }
+
+        // Specify HTTP method
+        /*[HttpGet]
+        public IActionResult Index(string name)
+        {
+            PersonModel person = new PersonModel(); // new Model.PersonModel();
+            person.FirstName = name;
+            return View(person);
+        }*/
+    }
+}
---
+++ Models/PersonModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool Modified { get; set; } = false; // Default value of false
+    }
+}
